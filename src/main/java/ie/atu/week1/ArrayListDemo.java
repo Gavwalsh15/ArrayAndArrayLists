bc45d4057fc298b60571ed8ee609bea8377f3607
Gavin <G00402296>
2023-01-30 12:33:59 +0000

    Got rid of Bulk :)

--- src/main/java/ie/atu/week1/ArrayListDemo.java
+++ src/main/java/ie/atu/week1/ArrayListDemo.java
@@ -9,21 +9,21 @@ public class ArrayListDemo {
         int i;
 
 
-        arrayList.add(0,100);
-        arrayList.add(1,200);
-        arrayList.add(2,300);
-        arrayList.add(3,400);
-        arrayList.add(4,500);
-        arrayList.add(5,600);
-        arrayList.add(6,700);
-        arrayList.add(7,800);
-        arrayList.add(8,900);
-        arrayList.add(9,1000);
+        arrayList.add(100);
+        arrayList.add(200);
+        arrayList.add(300);
+        arrayList.add(400);
+        arrayList.add(500);
+        arrayList.add(600);
+        arrayList.add(700);
+        arrayList.add(800);
+        arrayList.add(900);
+        arrayList.add(1000);
 
 
         System.out.println("Please enter a number:");
         Scanner input = new Scanner(System.in);
-        arrayList.add(10, input.nextInt());
+        arrayList.add(input.nextInt());
 
         for(i = 0; i < arrayList.size(); i++) {
             System.out.println("Element at index "+ i +": "+ arrayList.get(i));
